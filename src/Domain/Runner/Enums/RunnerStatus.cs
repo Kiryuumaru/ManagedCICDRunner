da608bf7b8d938df2423230b8b03dbcd7519aa41
Kiryuumaru <kiryuumaru@outlook.com>
2024-07-20 08:19:54 +0800

    Added dependabot

--- src/Domain/Runner/Enums/RunnerStatus.cs
+++ src/Domain/Runner/Enums/RunnerStatus.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
@@ -10,5 +11,15 @@ namespace Domain.Runner.Enums;
 [JsonConverter(typeof(JsonStringEnumConverter))]
 public enum RunnerStatus
 {
-    Building, Starting, Ready, Busy
+    [EnumMember(Value = "building")]
+    Building,
+
+    [EnumMember(Value = "starting")]
+    Starting,
+
+    [EnumMember(Value = "ready")]
+    Ready,
+
+    [EnumMember(Value = "busy")]
+    Busy
 }
