b96dcaaed6396ebc24ffea6e19726ca3cd0a9aa6
Clynt Neiko Rupinta <neiko@meldcx.com>
2024-07-22 15:53:54 +0800

    Fixed docker exec

--- src/Application/Docker/Services/DockerService.cs
+++ src/Application/Docker/Services/DockerService.cs
@@ -180,7 +180,7 @@ public class DockerService(ILogger<DockerService> logger)
     {
         return runnerOS switch
         {
-            RunnerOSType.Linux => "wsl docker",
+            RunnerOSType.Linux => "wsl docker --exec",
             RunnerOSType.Windows => "docker",
             _ => throw new NotSupportedException()
         };
