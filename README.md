ea33816b20664e4501217fb47b690c6cb4633465
Clynt Neiko Rupinta <31558769+Kiryuumaru@users.noreply.github.com>
2024-07-22 13:49:42 +0800

    Added one-line install (#8)
    * Added one-line install
    
    * Added one-line install

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ManagedCICDRunner
+
+```
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell -c "& ([ScriptBlock]::Create((irm https://github.com/Kiryuumaru/ManagedCICDRunner/releases/download/build.5/installer_windows_x64.ps1)))"
+```
---
+++ uninstallerTemplate.ps1
@@ -0,0 +1,30 @@
+$tag = '{{$tag}}'
+$repo = '{{$repo}}'
+$appname = '{{$appname}}'
+$appexec = '{{$appexec}}'
+$rootextract = '{{$rootextract}}'
+
+$root = (Get-Item .).FullName
+$tempPath = "$root\temp"
+
+$ErrorActionPreference = 'SilentlyContinue'
+New-Item -ItemType Directory -Path "$tempPath" -ErrorAction SilentlyContinue | Out-Null
+$ErrorActionPreference = 'Stop'
+
+$appZipName = "$appname.zip"
+$appPath = "$tempPath\$appname"
+$appZipPath = "$tempPath\$appZipName"
+$appExecPath = "$appPath\$appexec"
+
+if (Test-Path $appZipPath) {
+    Remove-Item -Path $appZipPath -Force
+}
+
+$appUri = "https://github.com/$repo/releases/download/$tag/$appZipName"
+Invoke-WebRequest -Uri $appUri -OutFile $appZipPath
+
+Expand-Archive -LiteralPath $appZipPath -DestinationPath $tempPath -Force
+
+Copy-Item -Path "$tempPath\$rootextract\*" -Destination "$root" -Recurse -Force
+
+& $appExecPath --uninstall-service
